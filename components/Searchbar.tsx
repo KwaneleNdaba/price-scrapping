01f2709c3698e9ac31d1ce1ffda89c390f5a2520
KwaneleNdaba <kwanelendaba113@gmail.com>
2023-12-15 00:30:54 +0200

    validating amazon product link

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,20 +1,86 @@
-"use client"
-import React from 'react'
-
+"use client";
+import React, { FormEvent, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function Searchbar() {
-  
-  const handleSubmit = () => {
+  const [searchPrompt, setSearchPrompt] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-  }
-    return (
-    <form onSubmit={handleSubmit}  className='flex flex-wrap gap-4 mt-12'>
-        <input type='text' placeholder='Enter product link' className='searchbar-input' />
+  const isValidAmazonProductUrl = (url: string) => {
+    try {
+      const parseUrl = new URL(url); //taking an argument making it a URL
+      const hostname = parseUrl.hostname; //checking where is the product url hosted
 
-        <button type='submit'  className='searchbar-btn'>
-            Search
+      if (
+        hostname.includes("amazon.com") ||
+        hostname.includes("amazon") ||
+        hostname.endsWith("amazon")
+      ) {
+        return true;
+      }
+    } catch (error) {
+      return false;
+    }
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const isValidProductLink = isValidAmazonProductUrl(searchPrompt);
+
+    let _id = toast.loading("Searching..", {
+      position: "top-center",
+      autoClose: 1000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+    if (!isValidProductLink) {
+      toast.update(_id, {
+        render: "Please enter a valid Amazon Product link",
+        type: "error",
+        isLoading: false,
+      });
+      setTimeout(() => {
+        toast.dismiss(_id);
+      }, 2000);
+      return false;
+    }
+
+    try {
+      setIsLoading(true);
+    } catch (error) {
+      toast.dismiss(_id);
+      console.log(error);
+    }
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchPrompt(e.target.value);
+  };
+  return (
+    <>
+      <ToastContainer />
+      <form onSubmit={handleSubmit} className="flex flex-wrap gap-4 mt-12">
+        <input
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Enter product link"
+          className="searchbar-input"
+        />
+
+        <button
+          disabled={searchPrompt === ""}
+          type="submit"
+          className="searchbar-btn"
+        >
+        {isLoading ? "Searching.." : "  Search"}
         </button>
-    </form>
-  )
+      </form>
+    </>
+  );
 }
 
-export default Searchbar
+export default Searchbar;
